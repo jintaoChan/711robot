d7904983b7e67d30dded25e01ef9f11911891967
jintao <jintao@todo.com>
2025-04-11 14:15:09 +0800

    TMPSAVE

--- src/TechControlInterface/bringup/launch/launch.py
+++ src/TechControlInterface/bringup/launch/launch.py
@@ -1,4 +1,4 @@
-# Copyright 2021 Stogl Robotics Consulting UG (haftungsbeschränkt)
+# Copyright 2023 ICube Laboratory, University of Strasbourg
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,30 +12,27 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler
-from launch.conditions import IfCondition
-from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+
     # Declare arguments
     declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
-            "gui",
-            default_value="true",
-            description="Start RViz2 automatically with this launch file.",
+            'description_file',
+            default_value='actuator.xacro',
+            description='URDF/XACRO description file with the axis.',
         )
     )
 
-    # Initialize Arguments
-    gui = LaunchConfiguration("gui")
+    description_file = LaunchConfiguration('description_file')
 
     # Get URDF via xacro
     robot_description_content = Command(
@@ -46,7 +43,7 @@ def generate_launch_description():
                 [
                     FindPackageShare("TechControlInterface"),
                     "urdf",
-                    "actuator.xacro",
+                    description_file,
                 ]
             ),
         ]
@@ -60,14 +57,11 @@ def generate_launch_description():
             "TechJoints.yaml",
         ]
     )
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("TechControlInterface"), "rviz", "urdf.rviz"]
-    )
 
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_controllers],
+        parameters=[robot_description, robot_controllers],
         output="both",
     )
     robot_state_pub_node = Node(
@@ -76,61 +70,40 @@ def generate_launch_description():
         output="both",
         parameters=[robot_description],
     )
-    rviz_node = Node(
-        package="rviz2",
-        executable="rviz2",
-        name="rviz2",
-        output="log",
-        arguments=["-d", rviz_config_file],
-        condition=IfCondition(gui),
-    )
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster"],
+        arguments=["joint_state_broadcaster", "-c", "/controller_manager"],
     )
 
-    robot_controller_spawner = Node(
+    trajectory_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=[
-                "--inactive",
-                "TechDisableController", 
-                "TechPositionController", 
-                "TechVelocityController", 
-                "--param-file", robot_controllers],
+        arguments=["trajectory_controller", "-c", "/controller_manager"],
     )
 
-    ui = Node(
-        package="TechRobotUI",
-        executable="TechRobotUI"
-    )
+    # velocity_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["velocity_controller", "-c", "/controller_manager"],
+    # )
 
-    # Delay rviz start after `joint_state_broadcaster`
-    delay_rviz_after_joint_state_broadcaster_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=joint_state_broadcaster_spawner,
-            on_exit=[rviz_node],
-        )
-    )
-
-    # Delay start of joint_state_broadcaster after `robot_controller`
-    # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
-    delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
-        event_handler=OnProcessExit(
-            target_action=robot_controller_spawner,
-            on_exit=[joint_state_broadcaster_spawner],
-        )
-    )
+    # effort_controller_spawner = Node(
+    #     package="controller_manager",
+    #     executable="spawner",
+    #     arguments=["effort_controller", "-c", "/controller_manager"],
+    # )
 
     nodes = [
         control_node,
         robot_state_pub_node,
-        robot_controller_spawner,
-        # delay_rviz_after_joint_state_broadcaster_spawner,
-        delay_joint_state_broadcaster_after_robot_controller_spawner,
-        # ui,
+        joint_state_broadcaster_spawner,
+        trajectory_controller_spawner,
+        # velocity_controller_spawner,
+        # effort_controller_spawner,
     ]
 
-    return LaunchDescription(declared_arguments + nodes)
+    return LaunchDescription(
+        declared_arguments +
+        nodes)
